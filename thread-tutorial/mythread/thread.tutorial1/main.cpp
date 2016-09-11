b2db0bc2928f477c769426e540b6639781d815e0
akinobu123 <akinobu.yamaguchi@gmail.com>
2016-09-11 22:14:27 +0900

    mod

--- thread-tutorial/mythread/thread.tutorial1/main.cpp
+++ thread-tutorial/mythread/thread.tutorial1/main.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 #include <unistd.h>
 #include "CRunnable.h"
-#include "CSynchronizer.h"
+#include "CMutex.h"
 
 static const char *const kThreadAName("threadA");
 static const char *const kThreadBName("threadB");
 
 int main()
 {
-    CSynchronizer *sync = CSynchronizer::createInstance();
+    CMutex *mutex = new CMutex();
  
-    CRunnable *aRunnable = CRunnable::createInstance(kThreadAName, sync);
+    CRunnable *aRunnable = CRunnable::createInstance(kThreadAName, mutex);
 
-    // step 3 implement here.
-    // create CRunnable instance.
-    CRunnable *bRunnable = CRunnable::createInstance(kThreadBName, sync);
+    CRunnable *bRunnable = CRunnable::createInstance(kThreadBName, mutex);
 
     sleep(1);
 
-    // step 3 implement here.
-    // delete CRunnable instance.
     delete bRunnable;
     
     delete aRunnable;
     
-    delete sync;
+    delete mutex;
 
     return 0;
 }
