0a1e876fe6c9a66925629877ba6014d70343cabe
akinobu123 <akinobu.yamaguchi@gmail.com>
2016-09-11 14:40:18 +0900

    mod

--- thread-tutorial/mythread/thread_lib2/CSynchronized.cpp
+++ thread-tutorial/mythread/thread_lib2/CSynchronized.cpp
@@ -10,54 +10,60 @@ CSynchronized::CSynchronized(CSynchronizer *sync)
 : fSync(sync)
 //, fCounter(0)
 {
+	::std::cout << "CSynchronized::construct() : before lock" << ::std::endl;
     if (fSync != 0) {
         fSync->lock();
 //        ++fCounter;
-//    	::std::cout << "CSynchronized::construct() : lock : counter=" << fCounter << ::std::endl;
-    	::std::cout << "CSynchronized::construct() : lock" << ::std::endl;
     } else {
         assert(false);
     }
+//    ::std::cout << "CSynchronized::construct() : lock : counter=" << fCounter << ::std::endl;
+	::std::cout << "CSynchronized::construct() : after lock" << ::std::endl;
 }
 
 CSynchronized::~CSynchronized()
 {
+	::std::cout << "CSynchronized::destruct() : before unlock " << ::std::endl;
     if (fSync != 0) {
 //        for(int i = 0; i < fCounter; ++i) {
-//        	::std::cout << "CSynchronized::destruct() : unlock " << i << ::std::endl;
-        	::std::cout << "CSynchronized::destruct() : unlock " << ::std::endl;
             fSync->unlock();
 //    }
     }
+//    ::std::cout << "CSynchronized::destruct() : unlock " << i << ::std::endl;
+	::std::cout << "CSynchronized::destruct() : after unlock " << ::std::endl;
 }
 
 // public member functions
 void CSynchronized::lock()
 {
+//    ::std::cout << "CSynchronized::lock() : counter=" << fCounter << ::std::endl;
+	::std::cout << "CSynchronized::lock() : before lock" << ::std::endl;
     if (fSync != 0) {
         fSync->lock();
 //        ++fCounter;
-//    	::std::cout << "CSynchronized::lock() : counter=" << fCounter << ::std::endl;
-    	::std::cout << "CSynchronized::lock() " << ::std::endl;
     }
+	::std::cout << "CSynchronized::lock() : after lock" << ::std::endl;
 }
 
 void CSynchronized::unlock()
 {
+	::std::cout << "CSynchronized::unlock() : before unlock" << ::std::endl;
     if (fSync != 0) {
 //        assert(fCounter > 0);
 //        --fCounter;
         fSync->unlock();
-//    	::std::cout << "CSynchronized::unlock() : counter=" << fCounter << ::std::endl;
-    	::std::cout << "CSynchronized::unlock()" << ::std::endl;
     }
+//    ::std::cout << "CSynchronized::unlock() : counter=" << fCounter << ::std::endl;
+	::std::cout << "CSynchronized::unlock() : after unlock" << ::std::endl;
 }
 
 void CSynchronized::wait()
 {
+	::std::cout << "CSynchronized::wait() : before wait" << ::std::endl;
     if (fSync != 0) {
         fSync->wait();
     }
+	::std::cout << "CSynchronized::wait() : after wait" << ::std::endl;
 }
 
 /*
@@ -72,7 +78,9 @@ bool CSynchronized::wait(int msec)
 
 void CSynchronized::notifyAll()
 {
+	::std::cout << "CSynchronized::notifyAll() : before notifyAll" << ::std::endl;
     if (fSync != 0) {
         fSync->notifyAll();
     }
+	::std::cout << "CSynchronized::notifyAll() : after notifyAll" << ::std::endl;
 }
